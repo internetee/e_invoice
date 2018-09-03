2f00c5acf1d7aa747ab87449cbd1c5b19efb3b3f
Artur Beljajev <artur.beljajev@internet.ee>
2018-09-03 17:49:26 +0300

    Reformat

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,5 @@
 require 'simplecov' if ENV['COVERAGE']
 
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "estonian_e_invoice"
-
-require "minitest/autorun"
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'estonian_e_invoice'
+require 'minitest/autorun'
