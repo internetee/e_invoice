c511700d2dc5dcc28314ba422973ea0d1047d167
Artur Beljajev <artur.beljajev@internet.ee>
2018-10-29 18:24:34 +0200

    Fix VAT

--- lib/estonian_e_invoice/vat/no_vat.rb
+++ lib/estonian_e_invoice/vat/no_vat.rb
@@ -4,6 +4,14 @@ module EstonianEInvoice
       def type
         'NOTTAX'
       end
+
+      def rate
+        0
+      end
+
+      def amount
+        0
+      end
     end
   end
 end
--- test/vat/no_vat_test.rb
+++ test/vat/no_vat_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class NoVATTest < Minitest::Test
+  def setup
+    @vat = EstonianEInvoice::VAT::NoVAT.new
+  end
+
   def test_returns_type
-    vat = EstonianEInvoice::VAT::NoVAT.new
-    assert_equal 'NOTTAX', vat.type
+    assert_equal 'NOTTAX', @vat.type
+  end
+
+  def test_returns_zero_rate
+    assert @vat.rate.zero?
+  end
+
+  def test_returns_zero_amount
+    assert @vat.amount.zero?
   end
 end
