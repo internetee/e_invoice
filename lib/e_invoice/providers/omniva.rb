da1768118251e2342effd8d00e0398145f548fcf
Artur Beljajev <artur.beljajev@internet.ee>
2019-05-22 17:25:42 +0300

    Refactor

--- lib/e_invoice/providers/omniva.rb
+++ lib/e_invoice/providers/omniva.rb
@@ -4,29 +4,37 @@ module EInvoice
       attr_reader :config
       attr_reader :soap_client
 
-      def initialize(user_config)
-        @config = OpenStruct.new(user_config)
+      def initialize(config)
+        @config = OpenStruct.new(config)
         @soap_client = Savon.client(wsdl: wsdl)
-      end
-
-      def test_mode?
-        config.test_mode
       end
 
       def wsdl
         if test_mode?
-          'https://testfinance.post.ee/finance/erp/erpServices.wsdl'
+          config.wsdl_test
         else
-          'https://finance.omniva.eu/finance/erp/erpServices.wsdl'
+          config.wsdl_production
         end
       end
 
       def deliver(e_invoice)
         message = normalize_e_invoice_xml(e_invoice.to_xml)
-        soap_client.call(:e_invoice, attributes: { authPhrase: config.password }, message: message)
+        soap_client.call(soap_operation, attributes: soap_attributes, message: message)
       end
 
       private
+
+      def soap_operation
+        config.soap_operation.to_sym
+      end
+
+      def soap_attributes
+        { authPhrase: config.password }
+      end
+
+      def test_mode?
+        config.test_mode
+      end
 
       def normalize_e_invoice_xml(xml)
         xml_doc = Nokogiri.XML(xml)
--- test/providers/omniva_test.rb
+++ test/providers/omniva_test.rb
@@ -17,42 +17,37 @@ end
 
 class OmnivaProviderTest < Minitest::Test
   def test_delivers_normalized_e_invoice
-    password = 'test-password'
-
     response_body = File.read('test/fixtures/omniva_wsdl.xml')
-    wsdl_request = stub_request(:get, 'https://finance.omniva.eu/finance/erp/erpServices.wsdl')
+    wsdl_request = stub_request(:get, 'https://provider.test/wsdl.xml')
                      .to_return(status: 200, body: response_body)
 
-    main_request = stub_request(:post, "https://finance.omniva.eu/finance/erp/")
-                     .with(body: main_request_body(password: password))
+    main_request = stub_request(:post, "https://provider.test/endpoint")
+                     .with(body: main_request_body)
                      .to_return(status: 200, body: '')
 
-
-    provider = EInvoice::Providers::Omniva.new(password: password)
+    provider = EInvoice::Providers::Omniva.new(wsdl_production: 'https://provider.test/wsdl.xml',
+                                               soap_operation: 'e_invoice',
+                                               password: 'test-password',
+                                               test_mode: false)
     provider.deliver(EInvoiceDouble.new)
 
     assert_requested wsdl_request
     assert_requested main_request
   end
 
-  def test_test_mode_is_off_by_default
-    provider = EInvoice::Providers::Omniva.new(password: 'any')
-    refute provider.test_mode?
-  end
-
   def test_uses_test_wsdl_when_test_mode_is_on
-    provider = EInvoice::Providers::Omniva.new(password: 'any', test_mode: true)
-    assert_equal 'https://testfinance.post.ee/finance/erp/erpServices.wsdl', provider.wsdl
+    provider = EInvoice::Providers::Omniva.new(test_mode: true, wsdl_test: 'wsdl-test')
+    assert_equal 'wsdl-test', provider.wsdl
   end
 
   def test_uses_production_wsdl_when_test_mode_is_off
-    provider = EInvoice::Providers::Omniva.new(password: 'any', test_mode: false)
-    assert_equal 'https://finance.omniva.eu/finance/erp/erpServices.wsdl', provider.wsdl
+    provider = EInvoice::Providers::Omniva.new(test_mode: false, wsdl_production: 'wsdl-production')
+    assert_equal 'wsdl-production', provider.wsdl
   end
 
   private
 
-  def main_request_body(password:)
-    "<?xml version=\"1.0\" encoding=\"UTF-8\"?><env:Envelope xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:tns=\"http://e-arvetekeskus.eu/erp\" xmlns:env=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ins0=\"http://www.pangaliit.ee/arveldused/e-arve/\"><env:Body><tns:EInvoiceRequest authPhrase=\"#{password}\"><E_Invoice/></tns:EInvoiceRequest></env:Body></env:Envelope>"
+  def main_request_body
+    '<?xml version="1.0" encoding="UTF-8"?><env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:tns="http://e-arvetekeskus.eu/erp" xmlns:env="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ins0="http://www.pangaliit.ee/arveldused/e-arve/"><env:Body><tns:EInvoiceRequest authPhrase="test-password"><E_Invoice/></tns:EInvoiceRequest></env:Body></env:Envelope>'
   end
 end
